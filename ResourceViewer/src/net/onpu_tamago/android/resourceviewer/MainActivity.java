d9eb6180583fedd17ecbf9ecf417287a4bb4693e
TakamiChie <chie@onpu-tamago.net>
2013-08-30 18:30:09 +0900

    ひとまずアニメーションリソースを一覧表示するタブを作成

---
+++ ResourceViewer/src/net/onpu_tamago/android/resourceviewer/MainActivity.java
@@ -0,0 +1,81 @@
+package net.onpu_tamago.android.resourceviewer;
+
+import net.onpu_tamago.android.resourceviewer.viewer.AnimationFragment;
+import android.app.ActionBar;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends FragmentActivity implements
+		ActionBar.OnNavigationListener {
+
+	/**
+	 * The serialization (saved instance state) Bundle key representing the
+	 * current dropdown position.
+	 */
+	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+	public static final String EXTRA_INDEX = "index";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Set up the action bar to show a dropdown list.
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+		// Set up the dropdown list navigation in the action bar.
+		Resources res = getResources();
+		actionBar
+				.setListNavigationCallbacks(
+						// Specify a SpinnerAdapter to populate the dropdown
+						// list.
+						new ArrayAdapter<String>(actionBar.getThemedContext(),
+								android.R.layout.simple_list_item_1,
+								android.R.id.text1,
+								res.getStringArray(R.array.title_fragments)),
+						this);
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		// Restore the previously serialized current dropdown position.
+		if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+			getActionBar().setSelectedNavigationItem(
+					savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+		}
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// Serialize the current dropdown position.
+		outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
+				.getSelectedNavigationIndex());
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int position, long id) {
+		// When the given dropdown item is selected, show its contents in the
+		// container view.
+		Fragment fragment = null;
+		switch (position) {
+		case 0:
+		case 1:
+			fragment = new AnimationFragment();
+			break;
+		default:
+			break;
+		}
+		Bundle args = new Bundle();
+		args.putInt(EXTRA_INDEX, position);
+		fragment.setArguments(args);
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.container, fragment).commit();
+		return true;
+	}
+
+}
---
+++ ResourceViewer/src/net/onpu_tamago/android/resourceviewer/viewer/AnimationFragment.java
@@ -0,0 +1,98 @@
+package net.onpu_tamago.android.resourceviewer.viewer;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import net.onpu_tamago.android.resourceviewer.MainActivity;
+import net.onpu_tamago.android.resourceviewer.R;
+import net.onpu_tamago.android.resourceviewer.classes.NameValuePair;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+
+import com.androidquery.AQuery;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * 
+ */
+public class AnimationFragment extends Fragment implements
+		OnItemSelectedListener {
+
+	private static final String TAG = "[Animation]ResourceViewer";
+	private View mView;
+	private ArrayList<NameValuePair> mIds;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		mView = inflater.inflate(R.layout.fragment_animation, container, false);
+		return mView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		Field[] fields = android.R.anim.class.getFields();
+		mIds = new ArrayList<NameValuePair>();
+		boolean animation = getArguments().getInt(MainActivity.EXTRA_INDEX, 0) == 0;
+
+		for (Field f : fields) {
+			try {
+				if (!animation || !f.getName().contains("interpolator")) {
+					Log.d(TAG, f.getName());
+					mIds.add(new NameValuePair(f.getName(), f.getInt(null)));
+				}
+			} catch (IllegalArgumentException e) {
+				throw new RuntimeException(e);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		AQuery $ = new AQuery(mView);
+
+		$.id(R.id.in_animationtype)
+				.adapter(
+						new ArrayAdapter<NameValuePair>(getActivity(),
+								android.R.layout.simple_spinner_dropdown_item,
+								mIds)).getSpinner()
+				.setOnItemSelectedListener(this);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> adapterview, View view,
+			int position, long id) {
+		NameValuePair pair = mIds.get(position);
+		
+		Animation animation;
+		if(pair.name.contains("interpolator")){
+			// インターポーレータ
+			animation = AnimationUtils.loadAnimation(getActivity(), R.anim.demo_animation);
+			animation.setInterpolator(getActivity(), pair.value);
+			animation.setRepeatMode(Animation.REVERSE);
+		}else{
+			// アニメーション
+			animation = AnimationUtils.loadAnimation(getActivity(), pair.value);
+			animation.setRepeatMode(Animation.REVERSE);
+		}
+		
+		AQuery $ = new AQuery(mView);
+		$.id(R.id.out_animate).animate(animation);
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> adapterview) {
+		onItemSelected(adapterview, null, 0, 0);
+	}
+
+}
